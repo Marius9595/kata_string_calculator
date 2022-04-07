2f5d6c965df9ebae8e0e8f687ba2938d6c25b662
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2022-04-07 22:14:57 +0100

    🔴  StringCalculatorShould test_work_with_a_single_number

--- string_calculator.py
+++ string_calculator.py
@@ -18,7 +18,7 @@ input  ->  output
 
 """
 
-#StringCalculatorShould test_translate_empties
+#StringCalculatorShould
 # red: 🔴
 # green:🟢
 class StringCalculatorShould(unittest.TestCase):
@@ -26,8 +26,12 @@ class StringCalculatorShould(unittest.TestCase):
     def setUp(self) -> None:
         self.string_calculator = StringCalculator()
 
-    def test_translate_empties(self):
-        self.assertEqual(0, self.string_calculator.add(""))  # add assertion here
+    def test_translate_empties_as_number(self):
+        self.assertEqual(0, self.string_calculator.add(""))
+
+    def test_work_with_a_single_number(self):
+        self.assertEqual(1, self.string_calculator.add("1"))
+        self.assertEqual(2, self.string_calculator.add("2"))
 
 
 if __name__ == '__main__':
