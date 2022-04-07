2bfc4504bdf2fd9b148c12304936095a2a75925a
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2022-04-07 22:30:33 +0100

    🔴 StringCalculatorShould test_handle_new_lines_between_numbers

--- string_calculator.py
+++ string_calculator.py
@@ -21,6 +21,8 @@ input  ->  output
  "2"   ->   2
  
  "1,2" ->   3
+ 
+ "1\n2,3" -> 6
 
 """
 
@@ -42,6 +44,9 @@ class StringCalculatorShould(unittest.TestCase):
     def test_add_all_numbers_identified(self):
         self.assertEqual(3, self.string_calculator.add("1,2"))
         self.assertEqual(14, self.string_calculator.add("5,9"))
+
+    def test_handle_new_lines_between_numbers(self):
+        self.assertEqual(3, self.string_calculator.add("1\n2,3"))
 
 
 if __name__ == '__main__':
